73a82411625387700b78cde4f6a28c9deedfba05
Mintu Giri <mintu.giri1997@gmail.com>
2021-07-18 00:26:24 +0530

    new build

--- Multiplier/Multiplier.h
+++ Multiplier/Multiplier.h
@@ -15,7 +15,7 @@ FOUNDATION_EXPORT const unsigned char MultiplierVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <Multiplier/PublicHeader.h>
 
-#import "Multiplier/Other.h"
+#import <Multiplier/Other.h>
 
 
 
